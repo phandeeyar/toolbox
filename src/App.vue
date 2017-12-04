68e9f9ec8b100a069f2b4d78d7ed73fb5bb16329
DragonVirus <limonster.li@gmail.com>
2017-12-04 18:49:04 +0630

    Init commit
    - VueJS Template with Bootstrap 4

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <div class="container text-center">
+      <h1>Phandeeyar Toolbox</h1>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+  @import "../node_modules/bootstrap/scss/bootstrap.scss";
+  @import "./assets/custom-style.scss";
+</style>
