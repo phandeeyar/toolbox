f838f5ae63bea3903a46770c7e2a163611128ba2
DragonVirus <limonster.li@gmail.com>
2017-12-05 11:01:29 +0630

    Move custom-style to assets/scss folder

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div class="container text-center">
       <h1>Phandeeyar Toolbox</h1>
-      
+      <router-view></router-view>
     </div>
   </div>
 </template>
@@ -15,5 +15,5 @@ export default {
 
 <style lang="scss">
   @import "../node_modules/bootstrap/scss/bootstrap.scss";
-  @import "./assets/custom-style.scss";
+  @import "./assets/scss/custom-style.scss";
 </style>
