c3d9d021d712ce708ed31fe401d4392f5bab1664
DragonVirus <limonster.li@gmail.com>
2017-12-05 11:02:04 +0630

    Add ToolList Component

---
+++ src/components/ToolList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="row" id="tool-list">
+    <div class="col-md-6 col-sm-12" v-for="tool in tools">
+      <div class="col-md-4"><img src="" :alt="tool.tools_name" width="100%"></div>
+      <div class="col-md-8">
+          <h3>{{ tool.tools_name }}</h3>
+          <p><strong>Difficulity : </strong>{{ tool.skill_level }}</p>
+          <p><strong>Use Cases : </strong>{{ tool.use_cases }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tools: []
+    }
+  },
+  methods: {
+
+  },
+  created () {
+    var parent = this
+    this.$tabletop.init({
+      key: '1b4izBhz69O3GaiJx1SL94PNRvaIiZFfq-BM9pMPtBD4',
+      callback: function (data, tabletop) {
+        parent.tools = data
+      },
+      simpleSheet: true
+    })
+  }
+}
+</script>
