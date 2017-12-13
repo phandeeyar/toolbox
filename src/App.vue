a9f5c79c8bd3f34cdff4b9fd7352d42028a264a8
DragonVirus <limonster.li@gmail.com>
2017-12-13 17:45:02 +0630

    Add bootstrap vue

--- src/App.vue
+++ src/App.vue
@@ -19,5 +19,6 @@ export default {
 
 <style lang="scss">
   @import "../node_modules/bootstrap/scss/bootstrap.scss";
+  @import "../node_modules/bootstrap-vue/dist/bootstrap-vue.css";
   @import "./assets/scss/custom-style.scss";
 </style>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import TableTop from '../node_modules/tabletop/src/tabletop.min.js'
+import BootstrapVue from 'bootstrap-vue'
 
 Vue.config.productionTip = false
 Vue.prototype.$tabletop = TableTop
+Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
 new Vue({
