96f939ef6314a5e9e775ed5d3d1206bedd6cefe7
DragonVirus <limonster.li@gmail.com>
2017-12-13 17:45:13 +0630

    Footer component : not used yet

---
+++ src/components/footer.vue
@@ -0,0 +1,15 @@
+<template>
+	<footer class="text-center">
+    <p>Phandeeyar Toolbox</p>
+		<rainbow-bar/>
+	</footer>
+</template>
+
+<script>
+import rainbowbar from './rainbowbar'
+export default {
+	components: {
+		'rainbow-bar': rainbowbar
+	}
+}
+</script>
